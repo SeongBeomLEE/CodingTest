64d6a7d826aed33280d1ef6631afcabe47b24244
SeongBeomLEE <2712qwer@naver.com>
2021-12-10 10:00:03 +0900

    211210

--- Sort_Algorithm/Merge_sort.py
+++ Sort_Algorithm/Merge_sort.py
@@ -1,6 +1,24 @@
 import random
 def merge_sort(arr):
-    return arr
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    l_arr = merge_sort(left)
+    r_arr = merge_sort(right)
+    i, j = 0, 0
+    result = []
+    while i < len(l_arr) and j < len(r_arr):
+        if l_arr[i] < r_arr[j]:
+            result.append(l_arr[i])
+            i += 1
+        else:
+            result.append(r_arr[j])
+            j += 1
+    result += l_arr[i:]
+    result += r_arr[j:]
+    return result
 
 arr = [i for i in range(1, 100)]
 random.shuffle(arr)
--- Sort_Algorithm/Quick_sort.py
+++ Sort_Algorithm/Quick_sort.py
@@ -1,6 +1,17 @@
 import random
 def quick_sort(arr):
-    return arr
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left_arr, equal_arr, right_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            left_arr.append(num)
+        elif num > pivot:
+            right_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(left_arr) + equal_arr + quick_sort(right_arr)
 
 arr = [i for i in range(1, 100)]
 random.shuffle(arr)
