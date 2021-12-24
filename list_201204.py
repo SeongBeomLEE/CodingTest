75f64b0de0f6d8634da3d21e9bd53f486eb698d9
SeongBeomLEE <2712qwer@naver.com>
2021-12-24 10:29:11 +0900

    211224

--- list_201204.py
+++ list_201204.py
@@ -1,7 +1,27 @@
-def solution(mylist):
-    answer = []
-    for i in mylist:
-        answer.append(len(i))
+# def solution(mylist):
+#     answer = []
+#     for i in mylist:
+#         answer.append(len(i))
+#     return answer
+# input = [[1],[2]]
+# print(solution(input))
+
+# 아쉽네... 이렇게 풀었으면 된건데....
+from collections import deque
+def solution(arr, s):
+    if sum(arr) < s: return 0
+    answer = 987654321
+    new_arr = deque([])
+    arr = deque(arr)
+    while arr:
+        new_arr.append(arr.popleft())
+        while sum(new_arr) >= s:
+            answer = min(len(new_arr), answer)
+            new_arr.popleft()
+
     return answer
-input = [[1],[2]]
-print(solution(input))
+
+arr = [1, 2, 3, 10, 11, 12, 14, 15, 1, 1]
+arr = [1, 1, 1, 1, 1]
+s = 1
+print(solution(arr, s))
