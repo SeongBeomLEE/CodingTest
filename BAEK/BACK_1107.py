6c34b5ec80dd7bba31993f58a009034d2e1b8844
SeongBeomLEE <2712qwer@naver.com>
2022-02-14 15:05:54 +0900

    2022-02-14

--- BAEK/BACK_1107.py
+++ BAEK/BACK_1107.py
@@ -4,7 +4,7 @@ broken_btn = input().split() if 1 <= int(input()) else []
 answer = abs(100 - N)
 
 # 모든 숫자 탐색 (완전 탐색)
-for num in range(1000001):
+for num in range(1000000):
     for n in str(num):
         # 해당 숫자 안에 고장난 숫자가 있다면 당연히 만들 수 없는 숫자이므로 사용 X
         if n in broken_btn:
