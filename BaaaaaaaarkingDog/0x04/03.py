3d4c0d90db36ba532382c797933c3903dd9e2353
SeongBeomLEE <2712qwer@naver.com>
2022-07-09 22:59:17 +0900

    [fix] 조건문 제거 모든 경우에 다 가능하도록 코드 수정

--- BaaaaaaaarkingDog/0x04/03.py
+++ BaaaaaaaarkingDog/0x04/03.py
@@ -8,34 +8,31 @@ input = sys.stdin.readline
 N, K = map(int, input().rstrip().split(" "))
 
 linked_list = {}
-
 answer = []
 
 for n in range(1, N + 1):
     if n == 1:
-        linked_list[n] = [N, n + 1]
+        linked_list[n] = [N, n]
     elif n == N:
-        linked_list[n] = [n - 1, 1]
+        linked_list[n] = [linked_list[n - 1][1], 1]
+        linked_list[n - 1][1] = n
     else:
-        linked_list[n] = [n - 1, n + 1]
+        linked_list[n] = [linked_list[n - 1][1], n]
+        linked_list[n - 1][1] = n
 
-if N < linked_list[N][1]:
-    answer = [1]
-
-else:
-    cnt = 1
-    now = 1
-    while linked_list:
-        if cnt % K == 0:
-            answer.append(now)
-            prev, next = linked_list[now]
-            linked_list[prev][1] = next
-            linked_list[next][0] = prev
-            del linked_list[now]
-            now = next
-            cnt += 1
-        else:
-            now = linked_list[now][1]
-            cnt += 1
+cnt = 1
+now = 1
+while linked_list:
+    if cnt % K == 0:
+        answer.append(now)
+        prev, next = linked_list[now]
+        linked_list[prev][1] = next
+        linked_list[next][0] = prev
+        del linked_list[now]
+        now = next
+        cnt += 1
+    else:
+        now = linked_list[now][1]
+        cnt += 1
 
 print("<" + ", ".join(list(map(str, answer))) + ">")
